975647e8347dd4b2e509bf0d058385b8a3cd919e
Robinson Webber <robinsonwebber@hotmail.com>
2026-04-05 19:30:47 -0300

    Set base path for deployment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/curriculo-computacao/',
   plugins: [react(), tailwindcss()],
 })
